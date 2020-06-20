6d14f5d14e9cddafeb1fba71d8a89c051de86a17
macaco6 <https://github.com/macaco6/python3.git>
2020-06-20 09:04:50 -0300

    pausei
    Signed-off-by: macaco6 <https://github.com/macaco6/python3.git>

---
+++ desafio_aula_4.py
@@ -0,0 +1,28 @@
+print("Olá, eu sou uma versão 'crua' da IA do meu mestre MAURICIO!")
+print("Irei fazer algumas perguntas e gostaria que me responde-se. OK?")
+print("APERTE ENTER PARA CONTINUAR!")
+agd = input("")
+
+nome = input("Digite seu nome: ")
+peso = input("E seu peso: ")
+
+print("Agora vamos ver sua data de nascimento! OK?")
+
+espera = input("Aperte enter!")
+
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = int(input("Ano: "))
+n1 = int(input("Qual ano voce se encontra?"))
+
+print("PRONTO!")
+print("Você nasceu dia:  {}/ {} / {} Neste dia você presenteou o MUNDO com sua pessoa!".format(dia, mes, ano))
+print("obrigado!")
+
+n2 = n1 - ano
+
+print("Se sua idade for correspondente a {} APERTE ENTER!".format(n2))
+
+espaço3 = input("")
+
+print("PRONTO! SEU CADASTRO FOI CONCLUIDO!")
